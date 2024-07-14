0bb99aaef98fd769d2b8468fc589918fad735325
Manuel Jimenez <manuel.jimenez@fleetcor.com>
2024-07-13 22:35:35 -0400

    update end of exploration

--- src/lib/directus.ts
+++ src/lib/directus.ts
@@ -1,7 +1,5 @@
 import { authentication, createDirectus, rest } from "@directus/sdk";
 
-const client = createDirectus("http://localhost:8055")
-  .with(authentication("cookie", { credentials: "include", autoRefresh: true }))
-  .with(rest({ credentials: "include" }));
+const client = createDirectus("http://localhost:8055").with(rest());
 
 export default client;
